78282a2ca1c42f10a48f26b00f0e8a05b12bf2df
AdryelKlever <adryel.klever@callidustecnologia.com.br>
2023-12-01 12:02:32 -0300

    Tipos de variaveis

---
+++ TiposVariaveis/main.go
@@ -0,0 +1,19 @@
+package main
+
+import "fmt"
+
+func main() {
+	// INTEIROS
+
+	var numero1 int8 = 100
+	var numero2 int16 = 10000
+	var numero3 int32 = 1000000000
+	var numero4 int64 = 1000000000000000000
+
+	fmt.Println(numero1)
+	fmt.Println(numero2)
+	fmt.Println(numero3)
+	fmt.Println(numero4)
+
+	// INTEIROS
+}
